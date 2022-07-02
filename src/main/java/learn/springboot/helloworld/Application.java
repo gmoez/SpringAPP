9f718c2d9198b31f804a91498e6ba3d7f484e8f8
Moez Gammoua <moezg@MBP-de-Moez.home>
2022-07-02 14:36:37 +0100

    change

--- src/main/java/learn/springboot/helloworld/Application.java
+++ src/main/java/learn/springboot/helloworld/Application.java
@@ -13,7 +13,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello world Spring app";
+        return "Hello world Spring app 1";
     }
 
     public static void main(String[] args) {
